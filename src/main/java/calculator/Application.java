8200cb8bdd85c3d14c01a65eda6e995ac04e29c8
minju0428 <sfhketum1231@gmail.com>
2025-10-19 13:18:49 +0900

    feat : 문자열 덧셈 기능 구현 및 커스텀 구분자 지원
    - 사용자 입력을 통해 문자열을 받아 숫자 덧셈 수행
    - 기본 구분자(, :)와 커스텀 구분자(//구분자\n숫자들) 파싱 처리
    - 양수만 유효한 값으로 계산하며, 음수나 잘못된 형식은 예외 처리
    - 유효하지 않은 입력 시 IllegalArgumentException 예외 발생

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,66 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String cal = Console.readLine();
+        String [] parts = split(cal);
+
+
+
+
+        int result = sum(parts);
+        System.out.print("결과 : " + result);
+
     }
+    private static String[] split(String cal ){
+        if(cal.startsWith("//")) {
+            int del = cal.indexOf("\\n");
+            if(del == -1) {
+                throw new IllegalArgumentException();
+            }
+            String custom = cal.substring(2, del);
+            String numbersPart = cal.substring(del + 2);
+            return numbersPart.split(custom);
+
+        }  else {
+            if (cal.isEmpty()) {
+                return new String[0];
+            } else {
+                return cal.split(",|:");
+            }
+        }
+    }
+
+    private static int sum(String[] parts){
+        int resultValue = 0;
+        for (int j=0; j<parts.length; j++){
+
+            if (parts[j].isEmpty()) continue;
+
+            try {
+                int number = Integer.parseInt(parts[j]);
+                if(number >0) {
+                    resultValue += number;
+                } else {
+                    throw new IllegalArgumentException();
+                }
+
+            } catch (NumberFormatException e){
+                throw new IllegalArgumentException();
+            }
+
+
+        }
+        return resultValue;
+    }
+
+
 }
+
